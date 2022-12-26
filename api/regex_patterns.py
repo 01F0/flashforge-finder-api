8ee8aa45deddfaa629de4d597806c90f8b975ee4
01F0 <01F0@users.noreply.github.com>
2022-12-26 11:13:58 +0100

    Made regex work with negative numbers.

--- api/regex_patterns.py
+++ api/regex_patterns.py
@@ -12,13 +12,13 @@ def regex_for_coordinates(field_name):
 def regex_for_current_temperature():
     """T0:210 /210 B:0 /0"""
 
-    return 'T0:([0-9].*?) '
+    return 'T0:(-?[0-9].*?) '
 
 
 def regex_for_target_temperature():
     """ T0:210 /210 B:0 /0 """
 
-    return '\/([0-9].*?) '
+    return '\/(-?[0-9].*?) '
 
 
 def regex_for_progress():
